c7c812aa41ee88bdc6e598eb6f977a963c54fb55
Esko Evtyukov (Desktop) <eskoonwheels@gmail.com>
2018-05-28 18:46:21 +0300

    Added empty project

---
+++ Assets/Scripts/Hex.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Hex
+{
+    int pos_x;
+    int pos_y;
+    HexType type;
+}
+
+public enum HexType
+{
+
+}
---
+++ Assets/Scripts/HexMap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HexMap {
+
+    Hex[][] hexes;
+
+    public void Initialize(int width, int height, GameObject hexPrefab, float hexDiameter, float dstBetweenHexes = 0f)
+    {
+        if (width <= 0 || height <= 0)
+            Debug.Log("Invalid parameters for hex map");
+        else
+        {
+
+        }
+    }
+
+    public void GenerateNewHexes()
+    {
+
+    }
+}
